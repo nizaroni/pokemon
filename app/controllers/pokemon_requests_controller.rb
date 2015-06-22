bdbfe0aff306b0850e4d05546a3f0835ac4df3b7
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-06-22 02:55:07 -0400

    Use new universal Pokemon API route

--- app/controllers/pokemon_requests_controller.rb
+++ app/controllers/pokemon_requests_controller.rb
@@ -3,24 +3,14 @@ require 'net/http'
 class PokemonRequestsController < ApplicationController
 	POKEMON_API_URL = "http://pokeapi.co/api/v1"
 
-	def get_info(urlString)
-		uri = URI.parse(urlString)
+	def get_info(url_string)
+		uri = URI.parse(url_string)
 		response = Net::HTTP.get_response(uri)
 		response.body
 	end
 
-	def pokedex
-		urlString = "#{POKEMON_API_URL}/pokedex/"
-		render json: get_info(urlString)
-	end
-
-	def pokemon
-		urlString = "#{POKEMON_API_URL}/pokemon/#{params[:pokemon_id]}/"
-		render json: get_info(urlString)
-	end
-
-	def move
-		urlString = "#{POKEMON_API_URL}/move/#{params[:move_id]}/"
-		render json: get_info(urlString)
+	def forward
+		url_string = "#{POKEMON_API_URL}/#{params[:uri]}/"
+		render json: get_info(url_string)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
 
   get "/pokedex", to: "static_pages#pokedex"
 
-  get "api/pokedex", to: "pokemon_requests#pokedex"
-  get "api/pokemon/:pokemon_id", to: "pokemon_requests#pokemon"
-  get "api/move/:move_id", to: "pokemon_requests#move"
+  get "/api/*uri", to: "pokemon_requests#forward"
 end
