ad400dfc9b718562f4ac1b9fceb0071581e30ed7
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-06-22 01:26:37 -0400

    Use double quotes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "static_pages#home"
 
-  get '/pokedex', to: 'static_pages#pokedex'
+  get "/pokedex", to: "static_pages#pokedex"
 
   get "api/pokedex", to: "pokemon_requests#pokedex"
   get "api/pokemon/:pokemon_id", to: "pokemon_requests#pokemon"
