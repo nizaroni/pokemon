b6bbb294e0c65e82499aa8816986d029bdda4b1e
Sharon <sharon.peishan.kennedy@gmail.com>
2015-07-12 21:07:23 +0200

    added stylesheet and sprite

--- app/assets/javascripts/pokemon_show_controller.js
+++ app/assets/javascripts/pokemon_show_controller.js
@@ -1,10 +1,26 @@
 var PokemonShowController = function (pokemon) {
   this.pokemon = pokemon;
-  this.$el = $("<p>");
+  this.$el = $("<div>", {class: "pokemon", html: ""});
 };
 
 PokemonShowController.prototype.render = function () {
-  this.$el.html(this.pokemon.name);
+  console.log("rendering pokemon");
+  console.log(this.pokemon);
+  var name = "<h2>" + this.pokemon.name + "</h2>";
+  if (this.pokemon.species !== "") {
+    var species = "<p>Species: " + this.pokemon.species + "</p>"
+  } else {
+    var species = "<p>Species: none </>";
+  };
+  if (this.pokemon.sprite) {
+    var sprite = "<p><img src='http://pokeapi.co" + this.pokemon.sprite.image + "' alt='sprite'></p>";
+  } else {
+    var sprite = "";
+  };
+  var height = "<p>Height: " + this.pokemon.height + "</p>";
+  var weight = "<p>Weight: " + this.pokemon.weight + "</p>";
+  var newHtml = sprite + name + species + height + weight;
+  this.$el.html(newHtml);
   return this;
 };
 
